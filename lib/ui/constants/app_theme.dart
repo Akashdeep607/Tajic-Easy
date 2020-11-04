38bcbf9403984c0474a78973575f6ea47e2fc90f
Akashdeep Singh <aksh.sd607@gmail.com>
2020-11-04 15:00:22 +0530

    Adding themes and constants

---
+++ lib/ui/constants/app_theme.dart
@@ -0,0 +1,19 @@
+/*
+Primary color - 631D76
+Secondary color - 272727 or 201a23
+Scaffold color - WHITE with 57% opacity
+
+BUTTONS
+Button Success color - 489f63
+Button other color - #d63031
+
+Text
+Heading Color - Black or 272727
+Subtitle - 707070 or
+
+TEXT_FORM_FIELD
+TextFormField Color  - EAEBF3
+TextFormField Border Color - 631D76
+Text and Icon Color for TfF - 707070
+TFF fill color - eaebf3
+*/
